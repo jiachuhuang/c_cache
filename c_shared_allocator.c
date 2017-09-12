34b0c2304ce058e724b54562ade5fc56dd794c70
黄家楚 <huangjiachu@huangjiachudeMacBook-Pro.local>
2017-09-12 22:47:56 +0800

    ccache alloc

--- c_shared_allocator.c
+++ c_shared_allocator.c
@@ -11,5 +11,13 @@
 #endif
 
 int c_cache_allocator_startup(void **p, c_shared_header **shared_header, c_shared_segment **shared_segments, const char *shared_name, unsigned long k_size, unsigned long v_size, char **error_in) {
-	
+	int ret;
+
+	ret = create_shmmap(p, shared_header, shared_segments, shared_name, k_size, v_size, error_in);
+
+	if(ret == C_CACHE_FAIL) {
+		return C_CACHE_FAIL;
+	}
+
+	return C_CACHE_OK;
 }
